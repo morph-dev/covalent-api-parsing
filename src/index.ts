68675e62ac87c86eb6a71a1203eb74be35feabab
morph <82043364+morph-dev@users.noreply.github.com>
2022-03-12 12:03:04 +0200

    Add printing

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,22 @@ import jsonFile from '../schemas/schemas.json'
 
 import { Api, JSONObject } from './model'
 import { parseApi } from './parsing'
+import { writeApi, writeCommonComponents } from './printing'
 import { getComponentSummaries } from './summary'
 
-function printSummaries(apis: Api[], include_single_version = false) {
+export function printSummaries(apis: Api[], single_version = true) {
+  if (single_version) {
+    console.log('Printing only single versions!')
+    console.log('count\ttoplevel\tname')
+    console.log('\t\t\ttoplevelpath')
+    console.log('-'.repeat(100))
+  } else {
+    console.log('Printing only non-single versions!')
+    console.log('count\tversions\tname')
+    console.log('\t\tv_count\tpath')
+    console.log('-'.repeat(100))
+  }
+
   const summaries = Object.values(getComponentSummaries(apis))
   summaries.sort((a, b) => {
     if (a.count != b.count) {
@@ -13,24 +26,75 @@ function printSummaries(apis: Api[], include_single_version = false) {
     return Object.keys(b.versions).length - Object.keys(a.versions).length
   })
   for (const summary of summaries) {
+    const name = summary.componentName
     const versions = Object.values(summary.versions)
-    if (!include_single_version && versions.length == 1) {
-      continue
+    if (single_version && versions.length == 1) {
+      const topLevelApis = versions[0].apis.filter((api) => name === api.mainComponentName)
+      console.log(`${summary.count}\t${topLevelApis.length}\t${name}`)
+      topLevelApis.map((api) => console.log(`\t\t\t${api.path}`))
+    } else if (!single_version && versions.length > 1) {
+      console.log([summary.count, versions.length, name].join('\t'))
+      for (const version of versions) {
+        console.log(
+          [
+            '',
+            '',
+            version.count,
+            version.apis.map((api) => ` ${api.mainComponentName} ${api.path}`).join('\n\t\t\t'),
+          ].join('\t')
+        )
+      }
     }
-    console.log([summary.count, versions.length, summary.componentName].join('\t'))
-    for (const version of versions) {
-      console.log(
-        [
-          '',
-          '',
-          version.count,
-          version.apis.map((api) => ` ${api.mainComponentName} ${api.path}`).join('\n\t\t\t'),
-        ].join('\t')
-      )
+  }
+  console.log()
+}
+
+export function findDuplicateMainComponents(apis: Api[]) {
+  const groupped: Record<string, Api[]> = {}
+  for (const api of apis) {
+    const name = api.mainComponentName
+    if (!(name in groupped)) {
+      groupped[name] = []
     }
+    groupped[name].push(api)
+  }
+
+  for (const name in groupped) {
+    if (groupped[name].length == 1) continue
+    console.log(name)
+    for (const api of groupped[name]) {
+      console.log(`\t${api.path}`)
+    }
+    console.log()
+  }
+}
+
+export function printToFile(apis: Api[], mainComponents: string[]) {
+  // Single version, non-top
+  const commonComponents = [
+    'ContractMetadata',
+    'Pagination',
+    'UniswapToken',
+    'UniswapTokenWithSupply',
+    'UniswapV2BalanceItem',
+  ]
+  writeCommonComponents(apis, commonComponents, 'common.ts')
+  for (const name of mainComponents) {
+    const matchingApis = apis.filter((api) => api.mainComponentName === name)
+    if (matchingApis.length > 1) {
+      throw Error(`Multiple apis found for name ${name}`)
+    }
+    if (matchingApis.length === 0) {
+      throw Error(`No apis found for name ${name}`)
+    }
+    const api = matchingApis[0]
+    writeApi(api, commonComponents)
   }
 }
 
 const json = jsonFile as unknown as JSONObject[]
 const apis = json.map(parseApi)
-printSummaries(apis)
+
+printSummaries(apis, true)
+// findDuplicateMainComponents(apis)
+printToFile(apis, ['AddressWithHistoricalPricesItem'])
--- src/printing.ts
+++ src/printing.ts
@@ -1,4 +1,103 @@
-import { Component, Property } from './model'
+import { Api, Component, Property } from './model'
+import * as fs from 'fs'
+
+function getPath(filename: string): string {
+  fs.mkdirSync('out', { recursive: true })
+  return `./out/${filename}`
+}
+
+export function writeCommonComponents(
+  apis: Api[],
+  commonComponentNames: string[],
+  filename = 'common.ts'
+) {
+  const commonComponents: Record<string, Api[]> = {}
+  for (const name of commonComponentNames) {
+    commonComponents[name] = apis.filter((api) => name in api.components)
+  }
+
+  const commonComponenStrings = Object.entries(commonComponents).map(([name, componentApis]) => {
+    // header
+    const topLevelApis = componentApis.filter((api) => api.mainComponentName === name)
+    if (topLevelApis.length > 1) {
+      throw Error(`Common component ${name} is top level for more than one API!`)
+    }
+    let header = ''
+    if (topLevelApis.length === 1) {
+      header = getHeaderString(topLevelApis[0])
+    }
+    // check component exists and unique
+    const componentStrings = componentApis.map((api) => componentToString(api.components[name]))
+    if (componentStrings.length === 0) {
+      throw Error(`No Apis found for component ${name}`)
+    }
+    if (componentStrings.some((s) => s !== componentStrings[0])) {
+      throw Error(`Multiple versions of component ${name}`)
+    }
+    return header + componentStrings[0]
+  })
+  const path = getPath(filename)
+  console.log(`Writing common components to ${path}`)
+  fs.writeFileSync(path, commonComponenStrings.join('\n'))
+}
+
+export function writeApi(
+  api: Api,
+  commonComponentNames: string[],
+  filename?: string,
+  commonFilename = 'common.ts'
+) {
+  if (filename === undefined) {
+    filename = `${api.mainComponentName}.ts`
+  }
+  if (commonFilename.endsWith('.ts')) {
+    commonFilename = commonFilename.slice(0, -3)
+  }
+
+  const sections = [] as string[]
+
+  const components = new Map(Object.entries(api.components))
+
+  // common components
+  const commonComponentsUsed = [] as string[]
+  for (const commonComponentName of commonComponentNames) {
+    if (components.has(commonComponentName)) {
+      commonComponentsUsed.push(commonComponentName)
+      components.delete(commonComponentName)
+    }
+  }
+  // import
+  if (commonComponentsUsed.length > 0) {
+    commonComponentsUsed.sort()
+    sections.push(`import { ${commonComponentsUsed.join(', ')} } from './${commonFilename}'\n`)
+  }
+
+  // mainComponent
+  const mainComponent = components.get(api.mainComponentName)
+  if (mainComponent === undefined) {
+    throw Error(`Main component ${api.mainComponentName} not found!`)
+  }
+  sections.push(getHeaderString(api) + componentToString(mainComponent))
+  components.delete(api.mainComponentName)
+
+  sections.push(...Array.from(components.values()).map(componentToString))
+
+  const path = getPath(filename)
+  console.log(`Writing api to ${path}`)
+  fs.writeFileSync(path, sections.join('\n'))
+}
+
+function getHeaderString(api: Api): string {
+  return [
+    `/**`,
+    ` * ${api.title}`,
+    ` *`,
+    ` * ${api.description}`,
+    ` *`,
+    ` * Path: \`${api.path}\``,
+    ` */\n`,
+  ].join('\n')
+}
 
 export function componentToString(component: Component): string {
   const lines = []
